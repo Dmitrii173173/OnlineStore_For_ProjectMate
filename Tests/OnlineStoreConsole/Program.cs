471f193fa4a0c96577519c3dbf6356d2502b4d3c
Dmitrii Ming <dmitrii.perfilev2020@gmail.com>
2022-06-09 20:47:21 +0300

    test console project

---
+++ Tests/OnlineStoreConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OnlineStoreConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
