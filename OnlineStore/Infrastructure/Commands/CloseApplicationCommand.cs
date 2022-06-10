9137c107386692617b1b594153c790ca77737423
Dmitrii Ming <dmitrii.perfilev2020@gmail.com>
2022-06-10 20:39:09 +0300

    Resource

---
+++ OnlineStore/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,14 @@
+﻿using OnlineStore.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace OnlineStore.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+    }
+}
